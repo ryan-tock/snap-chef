b8e772deb1a467236653dabedb8df7d6356f29c7
Juhar <jcsong06@gmail.com>
2025-02-15 15:51:07 -0800

    goodbye gemini

--- gemini_api.py
+++ gemini_api.py
@@ -3,7 +3,7 @@ from google.genai import types
 import PIL.Image
 import json
 
-model = "gemini-2.0-flash"
+model = "gemini-2.0-flash-latest"
 client = genai.Client(api_key="AIzaSyBG764Zn4BcmFCw2ZqkF8VEUwLYmUEZYvE")
 
 def parse_foods(foods_txt):
@@ -14,23 +14,28 @@ if __name__ == '__main__':
     # path = "images\\getty.jpg"
     image = PIL.Image.open(path)
 
-    food_response = client.models.generate_content(
+    """ food_response = client.models.generate_content(
         model=model,
         contents=["Identify all the foods and the number of each food in the fridge.", image]
-    )
+    ) """
 
-    foods_txt = food_response.text
-    print("Fridge contents:", foods_txt)
+    food_response = "There is an egg, 5 chickens, 10 apples, 1 pineapple, 1 cabbage, 3 beers, 1 lemonade, olive oil, salt"
 
-    recipe_prompt = (
+    #foods_txt = food_response.text
+    #print("Fridge contents:", foods_txt)
+    food_txt = food_response
+    print("Fridge contents:", food_txt)
+
+    """ recipe_prompt = (
         f"Based on the following list of foods and their quantities: {foods_txt}, "
         "please suggest recipes that use these ingredients. For each recipe, provide "
         "a list of ingredients with the required amounts, step-by-step cooking instructions, "
         "and indicate if any additional common ingredients are needed."
-    )
+    ) """
 
-    recipe_response = client.models.generate_content(
+    """ recipe_response = client.models.generate_content(
         model=model,
         contents=[recipe_prompt]
-    )
-    print("Recipe suggestions:", recipe_response.text)
+    ) """
+    recipe_response = "Recipe suggestions: 1. dd. d.d.d d.d.d d.d.d.d.d.d.d.ddddddddddddd"
+    #print("Recipe suggestions:", recipe_response.text)
